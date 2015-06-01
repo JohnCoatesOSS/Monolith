eb126a699ee340e7ca9ecbeec444f8166b3caed3
John Coates <acapulco1988@gmail.com>
2015-06-01 16:56:48 -0500

    build.rb updated
    changed to executable
    now checks for dpkg-deb being installed
    now installs dpkg-deb as necessary
    corrected path to control file

--- Templates/ObjectiveC Tweak/build.rb
+++ Templates/ObjectiveC Tweak/build.rb
@@ -1,4 +1,41 @@
+#!/usr/bin/ruby
 require 'fileutils'
+
+# check for dpkg-deb
+def which(cmd)
+  exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+  ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+    exts.each { |ext|
+      exe = File.join(path, "#{cmd}#{ext}")
+      return exe if File.executable?(exe) && !File.directory?(exe)
+    }
+  end
+  return nil
+end
+
+# install dpkg as necessary
+
+if which("dpkg-deb") == nil
+  puts "dpkg not detected, install? y/n"
+  response = gets.chomp
+  
+  if response[0] == "y"
+    if which("brew") == nil
+      puts "installing prerequisite: homebrew package manager"
+      system "ruby -e \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)\""
+    end
+    
+    puts "installing dpkg with homebrew"
+    system "brew install dpkg"
+
+  elsif response[0] == "n"
+    puts "install refused: cannot continue with build"
+    exit;
+  else
+    puts "Expected y or n, received: "+response
+    puts "cannot continue with build"
+  end
+end
 
 deviceIP = "192.168.1.161"
 
@@ -13,7 +50,7 @@ Dir.chdir(File.dirname(__FILE__)) do
     
     # copy over control file
     FileUtils.mkdir_p("./_/DEBIAN/")
-    FileUtils.copy_file(src="./DEBIAN/control", dst="./_/DEBIAN/control")
+    FileUtils.copy_file(src="../DEBIAN/control", dst="./_/DEBIAN/control")
     
     filename = "tweak.deb"
     system "dpkg-deb", "-b", "-Zgzip", "_", filename
